7bd659719341fbcdfe1285130d7c7ccba23481c1
sourabh <sp8046904@gmail.com>
2023-09-11 08:39:31 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import './App.css';
-import styled , {ThemeProvider} from 'styled-components';
-import {darkTheme} from "./Utils/Themes";
+import "./App.css";
+import styled, { ThemeProvider } from "styled-components";
+import { darkTheme } from "./Utils/Themes";
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/HeroSection";
-import Skills from "./Components/Skills"
+import Skills from "./Components/Skills";
+import Education from "./Components/Education";
 
 const Body = styled.div`
-background-color: ${({ theme }) => theme.bg};
-width: 100%;
-overflow-x: hidden;
+  background-color: ${({ theme }) => theme.bg};
+  width: 100%;
+  overflow-x: hidden;
+`;
+const Wrapper = styled.div`
+  background: linear-gradient(38.73deg, rgba(204, 0, 187, 0.15) 0%, rgba(201, 32, 184, 0) 50%), linear-gradient(141.27deg, rgba(0, 70, 209, 0) 50%, rgba(0, 70, 209, 0.15) 100%);
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%,30% 98%, 0 100%);
 `
-
 function App() {
   return (
     <ThemeProvider theme={darkTheme}>
-    <Navbar/>
-    <Body>
-    <Hero/>
-    <Skills/>
-Portfolio
-    </Body>
-
+      <Navbar />
+      <Body>
+        <Hero />
+        <Wrapper>
+          <Skills />
+          <Education />
+        </Wrapper>
+      </Body>
     </ThemeProvider>
   );
 }
